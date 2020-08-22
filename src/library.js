7502ff79551f8a82a275fd447755e9f9bdadbe6b
Hendoe <hendo1349@gmail.com>
2020-08-21 21:32:02 -0500

    first publish

--- src/library.js
+++ src/library.js
@@ -1,30 +1,30 @@
 import main from './index.js';
 
 let libraryItems = [
-    {
-        id: 'a',
-        title: "plumbing hard",
-        description: "chai tea bing bang wahoo plumbing all day swanging all night",
-        url: "hffbs://itsAme.luihio",
-        rating: 5,
-        expanded: true,
-    },
-    {
-        id: 'b',
-        title: "oh you bad",
-        description: "ladies love me and dogs wanna be me knowWhatIMean()",
-        url: "net.getiton.tonight",
-        rating: 3,
-        expanded: false,
-    },
-    {
-        id: "c",
-        title: "vidya guys",
-        description: "UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A START",
-        url: "vvv.realgamersnow.moc",
-        rating: 0,
-        expanded: false,
-    },
+    // {
+    //     id: 'a',
+    //     title: "plumbing hard",
+    //     description: "chai tea bing bang wahoo plumbing all day swanging all night",
+    //     url: "hffbs://itsAme.luihio",
+    //     rating: 5,
+    //     expanded: true,
+    // },
+    // {
+    //     id: 'b',
+    //     title: "oh you bad",
+    //     description: "ladies love me and dogs wanna be me",
+    //     url: "net.getiton.tonight",
+    //     rating: 3,
+    //     expanded: false,
+    // },
+    // {
+    //     id: "c",
+    //     title: "vidya guys",
+    //     description: "UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A START",
+    //     url: "vvv.realgamersnow.moc",
+    //     rating: 0,
+    //     expanded: false,
+    // },
 ];
 
 let filter = [
